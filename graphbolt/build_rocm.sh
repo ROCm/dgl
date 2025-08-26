b62b72c059460475a65d24149a5a8ae0de79e94d
jamesETsmith <james.smith9113@gmail.com>
2025-08-26 15:02:32 -0400

    With some manual tweaking graphbolt is compiling, but the python tests crash with missing symbols errors

--- graphbolt/build_rocm.sh
+++ graphbolt/build_rocm.sh
@@ -3,11 +3,13 @@
 
 #!/bin/bash
 # Helper script to build graphbolt libraries for PyTorch
-set -e
+set -euo pipefail
+GRAPHBOLT_SRCDIR=$(dirname $0)
+GRAPHBOLT_BINDIR=$BINDIR/graphbolt
+GRAPHBOLT_BUILD_DIR=$GRAPHBOLT_BINDIR
 
-mkdir -p build
-mkdir -p $BINDIR/graphbolt
-cd build
+mkdir -p $GRAPHBOLT_BUILD_DIR
+cd $GRAPHBOLT_BUILD_DIR
 
 if [ $(uname) = 'Darwin' ]; then
   CPSOURCE=*.dylib
@@ -19,17 +21,17 @@ CMAKE_FLAGS="-DROCMARCHS=${ROCMARCHS} -DUSE_HIP=$USE_HIP -DGPU_TARGETS=${ROCMARC
 echo "graphbolt cmake flags: $CMAKE_FLAGS"
 
 if [ $# -eq 0 ]; then
-  $CMAKE_COMMAND $CMAKE_FLAGS ..
-  cmake --build . --parallel
-  cp -v $CPSOURCE $BINDIR/graphbolt
+  $CMAKE_COMMAND $CMAKE_FLAGS -S $GRAPHBOLT_SRCDIR -B $GRAPHBOLT_BUILD_DIR
+  cmake --build $GRAPHBOLT_BUILD_DIR --parallel
+  # cp -v $CPSOURCE $GRAPHBOLT_BINDIR
 else
   for PYTHON_INTERP in $@; do
     TORCH_VER=$($PYTHON_INTERP -c 'import torch; print(torch.__version__.split("+")[0])')
     mkdir -p $TORCH_VER
     cd $TORCH_VER
-    $CMAKE_COMMAND $CMAKE_FLAGS -DPYTHON_INTERP=$PYTHON_INTERP ../..
-    cmake --build . --parallel
-    cp -v $CPSOURCE $BINDIR/graphbolt
+    $CMAKE_COMMAND $CMAKE_FLAGS -DPYTHON_INTERP=$PYTHON_INTERP -S $GRAPHBOLT_SRCDIR -B $GRAPHBOLT_BUILD_DIR
+    cmake --build $GRAPHBOLT_BUILD_DIR --parallel
+    # cp -v $CPSOURCE $GRAPHBOLT_BINDIR
     cd ..
   done
 fi
