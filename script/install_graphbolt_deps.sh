7da392ad2338694b66ba80ccf975c9a75f1c3f08
jamesETsmith <james.smith9113@gmail.com>
2025-11-14 12:25:32 -0500

    Adding links to rocprim PR and better comments about in the script to install graphbolt dependencies

--- script/install_graphbolt_deps.sh
+++ script/install_graphbolt_deps.sh
@@ -19,15 +19,23 @@ cmake -B build \
 cmake --build build --target install
 cd ${DEPS_DIR}
 
-# TODO this is an unacceptable way to do this, hopefully we can resolve https://github.com/ROCm/libhipcxx/issues/10 quickly
-# previous version of libhipcxx allowed semaphores and we didn't have a problems so we're turning them on here
-# find and remove all lines in the libhipcxx that contain "#error semaphore is not supported on AMD hardware and should not be included"
+# TODO this is an unacceptable way to do this,
+# see https://github.com/ROCm/libhipcxx/issues/10 for more details
+# This was implicitly not allowed in previous releases we were using, 
+# but with v2.7.0 they are explicitly not allowed.
+
+# We only use semaphores for a counter of IO operations in graphbolt, 
+# that only runs on the host (not on the device) so we should be "safe"
+# to use this for now.
 sed -i '/#error semaphore is not supported on AMD hardware and should not be included/d' ${INSTALL_PREFIX}/include/rapids/libhipcxx/cuda/semaphore
 sed -i '/#error semaphore is not supported on AMD hardware and should not be included/d' ${INSTALL_PREFIX}/include/rapids/libhipcxx/hip/semaphore
 sed -i '/#error semaphore is not supported on AMD hardware and should not be included/d' ${INSTALL_PREFIX}/include/rapids/libhipcxx/cuda/std/semaphore
 sed -i '/#error semaphore is not supported on AMD hardware and should not be included/d' ${INSTALL_PREFIX}/include/rapids/libhipcxx/hip/std/semaphore
 
 # TODO remove this once the patches are merged
+# the patches for this were merged in https://github.com/ROCm/rocm-libraries/pull/1883
+# but may take more time to be released.
+
 # Right now we need to patch the rocPRIM headers to fix the build because these
 # config headers are missing gfx942 (I've added them manually)
 cp ${FILE_SOURCE_DIR}/*.hpp ${INSTALL_PREFIX}/include/rocprim/device/detail/config/.
